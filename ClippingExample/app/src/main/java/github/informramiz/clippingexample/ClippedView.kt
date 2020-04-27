453c9339ba8bbf8ab80e1acdaf70b5a961b5ce68
Ramiz Raja <informramiz@gmail.com>
2020-04-27 23:20:07 +0500

    feat: Create blank custom view

---
+++ ClippingExample/app/src/main/java/github/informramiz/clippingexample/ClippedView.kt
@@ -0,0 +1,14 @@
+package github.informramiz.clippingexample
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * Created by Ramiz Raja on 27/04/2020
+ */
+class ClippedView @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyle: Int = 0
+) : View(context, attributeSet, defStyle)
